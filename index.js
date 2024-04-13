5b7e6238228a6f8d56add819458ac40ec8ebeece
JAVALAVALI <77857414+JAVALAVALI@users.noreply.github.com>
2024-04-13 15:52:33 -0600

    v2

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const nedb = require('nedb-promises')
 const app = express()
+
+let db = nedb.create('db')
+db.load('dbs.db').then(() => {
+    console.log("Data base cool");
+})
+
 
 const port = 4000 || process.env.PORT
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.send('<h1>Hola Kiro</h1>')
+    let date = new Date()
+    let ip = req.ip
+    db.insert({ ip, date })
 })
 
 app.listen(port, console.log(`Server on port ${port}`))
